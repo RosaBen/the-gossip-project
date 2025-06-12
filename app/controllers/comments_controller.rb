f2e5abb327215dcd5dd230c28b4bc2bbb3a31a88
RosaBen <benchabane.rosa@outlook.com>
2025-06-12 18:04:58 +0200

    edit and delete comment done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
-  # before_action :authenticate_user!  # 💡 devise or your own logic
-
+    before_action :set_gossip
+  before_action :set_comment, only: [ :edit, :update, :destroy ]
+  before_action :authenticate_user!
+  before_action :authorize_user!, only: [ :edit, :update, :destroy ]
   def create
     @gossip = Gossip.find(params[:gossip_id])
     @comment = @gossip.comments.build(comment_params)
@@ -14,7 +16,37 @@ class CommentsController < ApplicationController
     redirect_to gossip_path(@gossip)
   end
 
+    def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to gossip_path(@gossip), notice: "Commentaire mis à jour !"
+    else
+      render :edit, alert: "Erreur lors de la mise à jour."
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to gossip_path(@gossip), notice: "Commentaire supprimé."
+  end
+
   private
+
+  def set_gossip
+    @gossip = Gossip.find(params[:gossip_id])
+  end
+
+  def set_comment
+    @comment = @gossip.comments.find(params[:id])
+  end
+
+  def authorize_user!
+    unless current_user == @comment.user
+      redirect_to gossip_path(@gossip), alert: "Tu n'as pas le droit de modifier ce commentaire."
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
